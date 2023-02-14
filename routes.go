afe7e534073c67da6bdeb9bcf88450611e1ca173
xsdrt <m_redinger@hotmail.com>
2023-02-13 19:08:12 -0500

    For testing will add a randomString generator to helper.go in the  HiSpeed pkg;Then make the new test routes...

--- routes.go
+++ routes.go
@@ -17,7 +17,8 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
+	//Test routes for testing the user models with postgresql using inline funcs...
+	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) { //Test route to add a user...
 		u := data.User{
 			FirstName: "Michael",
 			LastName:  "Redinger",
@@ -28,11 +29,22 @@ func (a *application) routes() *chi.Mux {
 
 		id, err := a.Models.Users.Insert(u) //This should insert
 		if err != nil {
-			a.App.ErrorLog.Println(err)
+			a.App.ErrorLog.Println(err) //if an error will just print a blank "page" in th web browser...
 			return
 		}
 
 		fmt.Fprintf(w, "%d: %s", id, u.FirstName) //If this works should print out id and first name from the database after the insert
+	})
+
+	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
+		users, err := a.Models.Users.GetAll()
+		if err != nil {
+			a.App.ErrorLog.Println(err) //if an error will just print a blank "page" in the web browser...
+			return
+		}
+		for _, x := range users {
+			fmt.Fprint(w, x.LastName)
+		}
 	})
 
 	/* a.App.Routes.Get("/test-database", func(w http.ResponseWriter, r *http.Request) { //Temp route (inline func)...  //commented out inline test; saved for reference
--- vendor/github.com/xsdrt/hiSpeed/helpers.go
+++ vendor/github.com/xsdrt/hiSpeed/helpers.go
@@ -1,9 +1,28 @@
 package hiSpeed
 
 import (
+	"crypto/rand"
 	"os"
 )
 
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321_+"
+)
+
+// RandomString generates a random string length "n" from values in the const randomString
+func (h *HiSpeed) RandomString(n int) string { //Random string with "n" a number designating the length of the string...
+	s, r := make([]rune, n), []rune(randomString) //Just 2 slices of rune , a length s ="n" and a r = "randomString"...
+
+	//run a for loop..
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
+
+// Create a new directory if it does not exist...
 func (h *HiSpeed) CreateDirIfNotExist(path string) error {
 	const mode = 0755
 	if _, err := os.Stat(path); os.IsNotExist(err) {
@@ -16,6 +35,7 @@ func (h *HiSpeed) CreateDirIfNotExist(path string) error {
 	return nil
 }
 
+// Creates a new file at path if it does not exist...
 func (h *HiSpeed) CreateFileIfNotExists(path string) error {
 	var _, err = os.Stat(path)
 	if os.IsNotExist(err) {
