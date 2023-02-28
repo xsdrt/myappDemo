8f3cd4ec7e8c4d319af1e7fa11aa9fc5d18b552f
xsdrt <m_redinger@hotmail.com>
2023-02-28 17:54:07 -0500

    Create some tables in the integration_test dkr image and run some test...

--- data/integration_test.go
+++ data/integration_test.go
@@ -1,3 +1,7 @@
+//go:build integration
+
+// run tests with this command: go test . --tags integration --count=1
+
 package data
 
 import (
@@ -7,6 +11,9 @@ import (
 	"os"
 	"testing"
 
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/ory/dockertest/v3"
 	"github.com/ory/dockertest/v3/docker"
 )
@@ -72,6 +79,7 @@ func TestMain(m *testing.M) {
 		var err error
 		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName)) //Open up the connection to the database
 		if err != nil {
+			log.Println("Error:", err)
 			return err
 		}
 		return testDB.Ping() //Should keep pinging until the connection works properly (The ddb is built and waiting connections in the docker image)
@@ -80,4 +88,91 @@ func TestMain(m *testing.M) {
 		log.Fatalf("could not connect to docker: %s", err)
 	}
 
+	err = createTables(testDB)
+	if err != nil {
+		log.Fatalf("error creating tables: %s", err)
+	}
+
+	models = New(testDB)
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func createTables(db *sql.DB) error {
+	stmt := `
+	CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+drop table if exists users cascade;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(255) NOT NULL,
+    last_name character varying(255) NOT NULL,
+    user_active integer NOT NULL DEFAULT 0,
+    email character varying(255) NOT NULL UNIQUE,
+    password character varying(60) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
+
+drop table if exists remember_tokens;
+
+CREATE TABLE remember_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    remember_token character varying(100) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON remember_tokens
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
+
+drop table if exists tokens;
+
+CREATE TABLE tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    first_name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    token character varying(255) NOT NULL,
+    token_hash bytea NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    expiry timestamp without time zone NOT NULL
+);
+
+CREATE TRIGGER set_timestamp
+    BEFORE UPDATE ON tokens
+    FOR EACH ROW
+    EXECUTE PROCEDURE trigger_set_timestamp();
+	`
+
+	_, err := db.Exec(stmt)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func TestUser_Table(t *testing.T) {
+	s := models.Users.Table()
+	if s != "users" {
+		t.Error("wrong table name returned: ", s)
+	}
 }
