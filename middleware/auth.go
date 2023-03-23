f0ba4b763a7ca6cfb475bf3337be291aea3d7144
xsdrt <m_redinger@hotmail.com>
2023-03-22 22:52:17 -0400

    tested adding  middleware, and files from the HiSpeed framework using the cli and make auth; worked like a charm.

--- middleware/auth.go
+++ middleware/auth.go
@@ -7,7 +7,7 @@ import (
 func (m *Middleware) Auth(next http.Handler) http.Handler { //web based auth only, need api also
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !m.App.Session.Exists(r.Context(), "userID") {
-			http.Error(w, http.StatusText(401), http.StatusUnauthorized)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 		}
 	})
 }
